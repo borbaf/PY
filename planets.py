7f4d74bc7e3405106adcba03ef8d2ca370e94057
Filippe Borba <borbaf@gmail.com>
2026-07-02 11:09:09 -0300

    "Teste alteração utilizando GITHUB"

--- planets.py
+++ planets.py
@@ -1,9 +1,1 @@
-planets = [Earth, Mars, Jupiter, Saturn, Uranus, Neptune]
-
-for planet in planets:
-    print("Planet:", planet)
-
-
-
-
-    i += 1
+planets = ["Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
