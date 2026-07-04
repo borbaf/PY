9261af1d83a579f5d589a1f8885771af3883b4dd
Filippe Borba <borbaf@gmail.com>
2026-07-04 11:25:32 -0300

    Banco de Dados SQL com PostgreSQL

---
+++ M4/main.py
@@ -0,0 +1,27 @@
+import psycopg2
+from psycopg2 import sql
+
+
+conn = psycopg2.connect(
+    host="localhost",
+    port=5432,
+    dbname="postgres",
+    user="borbaf",
+    password="12345678"
+)
+
+conn.autocommit = True
+
+#definir novo Banco de Dados
+db_name = "db_borbaf"
+
+#Criar uma sting SQL para ser executada
+create_db_query = sql.SQL("CREATE DATABASE {}").format(sql.Identifier(db_name))
+
+#Construindo um cursor para executar a query
+cursor = conn.cursor()
+cursor.execute(create_db_query)
+
+#fechar o cursor e a conexão
+cursor.close()
+conn.close()
