a12e3a8dd2952a68944b5d5ff4c8016464f0c763
Filippe Borba <borbaf@gmail.com>
2026-07-04 13:13:49 -0300

    Select Update and Delete

--- M4/main.py
+++ M4/main.py
@@ -5,7 +5,7 @@ from psycopg2 import sql
 conn = psycopg2.connect(
     host="localhost",
     port=5432,
-    dbname="postgres",
+    dbname="db_borbaf",
     user="postgres",
     password="P$inUca01"
 )
@@ -13,17 +13,69 @@ conn = psycopg2.connect(
 conn.autocommit = True
 
 #definir novo Banco de Dados
-db_name = "db_borbaf"
+#db_name = "postgres"
 
 #Criar uma sting SQL para ser executada
-create_db_query = sql.SQL("CREATE DATABASE {}").format(sql.Identifier(db_name))
+#create_db_query = sql.SQL("CREATE DATABASE {}").format(sql.Identifier(db_name))
 
 #Construindo um cursor para executar a query
-cursor = conn.cursor()
-cursor.execute(create_db_query)
+#cursor = conn.cursor()
+#cursor.execute(create_db_query)
 
 #fechar o cursor e a conexão
+#cursor.close()
+#conn.close()
+
+#print(f"Banco de dados '{db_name}' criado com sucesso!")
+
+
+# create_table_query = '''
+#     CREATE TABLE nome_tabela (
+#     coluna1 VARCHAR(255),
+#     coluna2 VARCHAR(255)
+#     )
+# '''
+# cursor = conn.cursor()
+# cursor.execute(create_table_query)
+# conn.commit()
+
+# cursor.close()
+# conn.close()
+
+# print("Tabela criada com sucesso!")
+
+
+def create_table(connection):
+    """Executa o CREATE TABLE corrigido no banco de dados."""
+    cursor = connection.cursor()
+    cursor.execute(CREATE_TABLE_SQL)
+    connection.commit()
+    cursor.close()
+
+
+
+#INSERT
+
+insert_query = '''
+    INSERT INTO nome_tabela (coluna1, coluna2) VALUES (%s, %s)
+'''
+cursor = conn.cursor()
+cursor.execute(insert_query, ("valor1", "valor2"))
+conn.commit()
+
 cursor.close()
 conn.close()
 
-print(f"Banco de dados '{db_name}' criado com sucesso!")
+print("Dados inseridos com sucesso!")
+
+
+#SELECT
+
+cur.execute("SELECT * FROM nome_tabela")
+rows = cur.fetchall()
+
+for row in rows:
+    print(row)
+
+cur.close()
+conn.close()
