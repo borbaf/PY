3223b2c3507591f26c2edf3adecc1920ed43c7ea
Filippe Borba <borbaf@gmail.com>
2026-07-07 16:49:30 -0300

    ADd comentarios para o código e corrige a leitura do arquivo CSV com encoding adequado.

--- DESAFIO_C2/main.py
+++ DESAFIO_C2/main.py
@@ -1,17 +1,24 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import os
+
+# Obtém o caminho absoluto do diretório onde este script está localizado.
+# __file__ é o caminho do próprio arquivo .py, os.path.abspath garante o caminho
+# completo e os.path.dirname extrai apenas a pasta (sem o nome do arquivo).
+
+diretorio_script = os.path.dirname(os.path.abspath(__file__)) 
 
 
-diretorio_script = os.path.dirname(os.path.abspath(__file__))
-csv_path = os.path.join(diretorio_script, "Sample - Superstore.csv")
+# Junta o diretório do script com o nome do arquivo CSV, criando um caminho
+# completo e independente de onde o terminal esteja executando o comando.
 
-df = pd.read_csv(csv_path)
+csv_path = os.path.join(diretorio_script, "Sample - Superstore.csv") # explicita o caminho do arquivo csv
 
 
 # Carregar o dataset
 try:
-    df = pd.read_csv(csv_path)
+    df = pd.read_csv(csv_path,encoding='cp1252')
     print("Dataset carregado com sucesso!")
     print(f"Formato: {df.shape}")
     print(df.head())
